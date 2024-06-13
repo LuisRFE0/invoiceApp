10813a3552c2aa63674c68d045762af4c19f520c
Luis flores <luis.r.f.e@hotmail.com>
2024-06-12 19:06:12 -0600

    Funcion get invoice

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { invoice } from '../data/invoice'
+import { getInvoice } from '../services/getInvoice'
 
 export const InvoiceApp = () => {
+
+    const invoice = getInvoice();
     return (
         <>
 
             <h1>Factura</h1>
+            <ul>
+                <li>ID: {invoice.id}</li>
+                <li>Name: {invoice.name}</li>
+            </ul>
 
         </>
     )
